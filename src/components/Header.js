321b585b0e4cf93c7b8c8181b7ca1103f443da5c
un-earthly <vijayalamin@gmail.com>
2024-08-14 00:24:41 +0600

    udpates on link

--- src/components/Header.js
+++ src/components/Header.js
@@ -548,7 +548,7 @@ const ListItem = forwardRef(({ className, title, children, ...props }, ref) => {
     return (
         <li>
             <NavigationMenuLink asChild>
-                <a
+                <Link
                     ref={ref}
                     className={cn(
                         "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
@@ -560,7 +560,7 @@ const ListItem = forwardRef(({ className, title, children, ...props }, ref) => {
                     <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
                         {children}
                     </p>
-                </a>
+                </Link>
             </NavigationMenuLink>
         </li>
     );
